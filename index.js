773fdb033472c61416c890d4894e00c629935856
loganrun <bigdog_41@hotmai.com>
2024-02-02 14:18:44 -0800

    added companion

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const adventurer = {
         companion: {
             name: "Frank",
             type: "Flea",
-            inventory: ["hat", "sunglasses"]
+            inventory: ["small hat", "sunglasses"]
         }
     },
 
@@ -19,14 +19,44 @@ const adventurer = {
     }
     }
     
-    adventurer.roll();
+    //adventurer.roll();
 
 //Class Fantasy
 
 class Character {
     constructor (name) {
-      this.name = name;
-      this.health = 100;
-      this.inventory = [];
+    this.name = name;
+    this.health = 100;
+    this.inventory = [];
+    }
+}
+
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = 'Flea';
+robin.companion.companion.inventory = ["small hat", "sunglasses"]
+
+// class features
+
+class Adventurer extends Character {
+    constructor (name, role) {
+      super(name);
+      this.role = role;
+      this.inventory.push("bedroll", "50 gold coins");
+    }
+    scout () {
+      console.log(`${this.name} is scouting ahead...`);
+      super.roll();
+    }
+  }
+
+  class Companion extends Character {
+    constructor (name, role) {
+      super(name);
+      this.role = role;
+      this.inventory.push("sword", "potion", "artifact");
     }
   }
